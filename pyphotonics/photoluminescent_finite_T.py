9ae59e099e205cddb3e8b853a3d82aae66d0e6f9
Ngoc Linh Nguyen <ngoclinh84phys@gmail.com>
2023-07-31 21:54:19 +0700

    Update photoluminescent_finite_T.py

--- pyphotonics/photoluminescent_finite_T.py
+++ pyphotonics/photoluminescent_finite_T.py
@@ -231,8 +231,10 @@ class Photoluminescence:
             #self.frequencies[i] is already in eV unit
             #1 kelvin = 0.00008617328149741 electron-volt
             if temp > 0.0:
-                number_occ_phonon_i = 1.0/(np.exp((self.frequencies[i])/\
-                    (0.00008617328149741 * temp)) - 1.0)
+                if self.frequencies[i] == 0:
+                    number_occ_phonon_i = 0.0
+                else:
+                    number_occ_phonon_i = 1.0/(np.exp(self.frequencies[i]/(0.00008617328149741 * temp)) - 1.0)
             else:
                 number_occ_phonon_i = 0.0
             C_i =  number_occ_phonon_i*S_i   
